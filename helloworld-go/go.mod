35f8e4045e3532a4e48097a71acd2ccfe31bc705
Izabela Gomes <igomes@vmware.com>
2020-12-09 11:39:25 -0500

    add hello-world app for knative
    hello-world app for knative based on
    https://github.com/knative/docs/tree/master/docs/serving/samples/hello-world/helloworld-go
    
    Signed-off-by: Izabela Gomes <igomes@vmware.com>

---
+++ helloworld-go/go.mod
@@ -0,0 +1,15 @@
+module github.com/izabelacg/palm-tree/helloworld-go
+
+go 1.15
+
+require (
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0 // indirect
+	github.com/tuvistavie/securerandom v0.0.0-20140719024926-15512123a948 // indirect
+	github.com/wavefronthq/go-metrics-wavefront v1.0.2 // indirect
+	github.com/wavefronthq/wavefront-opentracing-sdk-go v0.9.0
+	github.com/wavefronthq/wavefront-sdk-go v0.9.7
+)
---
+++ helloworld-go/helloworld.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/wavefronthq/wavefront-opentracing-sdk-go/reporter"
+	"github.com/wavefronthq/wavefront-opentracing-sdk-go/tracer"
+	"github.com/wavefronthq/wavefront-sdk-go/application"
+	"github.com/wavefronthq/wavefront-sdk-go/senders"
+
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("helloworld: received a request")
+
+	// From the example https://github.com/opentracing/opentracing-go
+
+	var serverSpan opentracing.Span
+	appSpecificOperationName := "print-hello"
+	wireContext, err := opentracing.GlobalTracer().Extract(
+		opentracing.HTTPHeaders,
+		opentracing.HTTPHeadersCarrier(r.Header))
+	if err != nil {
+		// Optionally record something about err here
+		log.Printf("Couldn't extract information from headers: %s\n", err.Error())
+	}
+
+	// Create the span referring to the RPC client if available.
+	// If wireContext == nil, a root span will be created.
+	serverSpan = opentracing.StartSpan(
+		appSpecificOperationName,
+		ext.RPCServerOption(wireContext))
+
+	defer serverSpan.Finish()
+
+	opentracing.ContextWithSpan(context.Background(), serverSpan)
+
+	// -- end
+
+	time.Sleep(time.Second * 30)
+
+	target := os.Getenv("TARGET")
+	if target == "" {
+		target = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", target)
+}
+
+func main() {
+	log.Print("helloworld: starting server...")
+
+	NewGlobalTracer("kn-hello-app", "kn-hello-service")
+
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("helloworld: listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func NewGlobalTracer(applicationName string, serviceName string) io.Closer {
+
+	// From: https://demo.wavefront.com/integration/go/setup
+	// and https://github.com/wavefrontHQ/wavefront-opentracing-sdk-go
+
+	directCfg := &senders.DirectConfiguration{
+		Server: "https://vmware.wavefront.com",
+		Token:  "",
+	}
+
+	sender, err := senders.NewDirectSender(directCfg)
+	if err != nil {
+		panic(err)
+	}
+
+	appTags     := application.New(applicationName, serviceName)
+	wfReporter := reporter.New(sender, appTags, reporter.Source("hello-tracing.serverless-demo.example.com.com"))
+	clReporter := reporter.NewConsoleSpanReporter("hello-tracing.serverless-demo.example.com.com") //Specify the same source you used for the WavefrontSpanReporter
+	reporter := reporter.NewCompositeSpanReporter(wfReporter, clReporter)
+	tracer      := tracer.New(reporter)
+
+	opentracing.SetGlobalTracer(tracer)
+
+	return ioutil.NopCloser(nil)
+
+}
